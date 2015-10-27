1eaa1788dd650693ff443d35459263b01e3516fb
Zhang Jiaofa <zjf@haoju.cn>
2015-10-27 10:56:10 +0800

    增加AssertionFailedError的类相关的描述

--- src/main/java/junit/framework/AssertionFailedError.java
+++ src/main/java/junit/framework/AssertionFailedError.java
@@ -2,6 +2,7 @@ package junit.framework;
 
 /**
  * Thrown when an assertion failed.
+ * 当一个断言失败的时候抛出
  */
 public class AssertionFailedError extends AssertionError {
 
@@ -9,12 +10,14 @@ public class AssertionFailedError extends AssertionError {
     
     /**
      * Constructs a new AssertionFailedError without a detail message.
+     * 没有异常信息描述的断言异常的对象的构建
      */
     public AssertionFailedError() {
     }
 
     /**
      * Constructs a new AssertionFailedError with the specified detail message.
+     * 用指定的详细的信息构造一个新的断言异常的对象
      * A null message is replaced by an empty String.
      * @param message the detail message. The detail message is saved for later 
      * retrieval by the {@code Throwable.getMessage()} method.
